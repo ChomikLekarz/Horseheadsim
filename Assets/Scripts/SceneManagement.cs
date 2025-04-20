6e81935b1500bf09012e6a33a0902beea9eb48c3
ChomikLekarz <haunteredpl@gmail.com>
2025-04-20 22:02:23 +0200

    A buncha things added

---
+++ Assets/Scripts/SceneManagement.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.InputSystem;
+
+public class SceneManagement : MonoBehaviour
+{
+    public int ButtonNumber;
+    float press = Pointer.current.press.ReadValue();
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        switch (ButtonNumber)
+        {
+            case 1:
+                SceneManager.LoadScene(sceneName: "Area 1");
+                break;
+            case 2:
+                SceneManager.LoadScene(sceneName: "Area 2");
+                break;
+            case 3:
+                SceneManager.LoadScene(sceneName: "Area 3");
+                break;
+            case 4:
+                SceneManager.LoadScene(sceneName: "Area 4");
+                break;
+            case 5:
+                SceneManager.LoadScene(sceneName: "Area 5");
+                break;
+
+        }
+    }
+}
